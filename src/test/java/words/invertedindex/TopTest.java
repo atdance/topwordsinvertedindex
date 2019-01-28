8fb66b54e0a3f329bc8beb25b93a825f8f80ae29
atdance1 <miccia2@mail.com>
2019-01-28 20:19:19 +0100

    added setUp()

--- src/test/java/words/invertedindex/TopTest.java
+++ src/test/java/words/invertedindex/TopTest.java
@@ -11,12 +11,21 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TopTest {
 
-	@Test
-	public void shouldReturnTheTenMostFrequentWords() {
+	List<Word> expected;
+
+	List<Word> expectedBase;
+
+	List<Word> extraWords;
+
+	List<Word> wordsUnderTest = new ArrayList<>();
+
+	@Before
+	public void setUp() {
 		final Word a = new Word("a");
 		final Word b = new Word("b");
 		final Word c = new Word("c");
@@ -28,25 +37,26 @@ public class TopTest {
 		final Word i = new Word("i");
 		final Word m = new Word("m");
 
-		final List<Word> expectedBase = Arrays.asList(a, a, a, b, b, b, c, c, d, d, e, e, f, f, g, g, g, h, h, i, i, m,
-				m);
+		expected = Arrays.asList(a, b, g, c, d, e, f, h, i, m);
 
-		final List<Word> expected = Arrays.asList(a, b, g, c, d, e, f, h, i, m);
+		expectedBase = Arrays.asList(a, a, a, b, b, b, c, c, d, d, e, e, f, f, g, g, g, h, h, i, i, m, m);
 
-		final List<Word> extraWords = Arrays.asList(new Word("zzzz"), new Word("uuuuu"), new Word("vvvvv"),
-				new Word("tttttt"), new Word("xxxxxx"), new Word("yyyyy"));
+		extraWords = Arrays.asList(new Word("zzzz"), new Word("uuuuu"), new Word("vvvvv"), new Word("tttttt"),
+				new Word("xxxxxx"), new Word("yyyyy"));
 
-		List<Word> wordsUnderTest = new ArrayList<>();
+		wordsUnderTest = new ArrayList<>();
 
 		wordsUnderTest.addAll(expectedBase);
 
 		wordsUnderTest.addAll(extraWords);
 
 		Collections.shuffle(wordsUnderTest);
+	}
 
-		InvertedIndex<Word> index = new InvertedIndex<>();
+	@Test
+	public void shouldReturnTheTenMostFrequentWords() {
 
-		Words<Word> frequencyTable = index.getWords(wordsUnderTest);
+		Words<Word> frequencyTable = new InvertedIndex<Word>().getWords(wordsUnderTest);
 
 		Map<Integer, HashSet<Word>> firstTen = Top.getTopTen(frequencyTable, 10);
 
